54432d4b5fdcc3b65884d6de22b98c14ab3bf83e
Grant Klimaytys <grantmk@users.noreply.github.com>
2024-02-29 08:38:20 +0000

    Update index.ts

--- src/routes/pokemon/index.ts
+++ src/routes/pokemon/index.ts
@@ -4,18 +4,7 @@ import { createPokemon, getAllPokemon, getPokemon } from './handlers';
 const pokemonRoutes = new Elysia({ prefix: '' })
   .get('/', () => getAllPokemon)
   .get('/:id', ({ params: { id } }) => getPokemon(id))
-  .post('/', ({ body }) => createPokemon(body), {
-    body: t.Object({
-      name: t.String({
-        minLength: 3,
-        maxLength: 50,
-      }),
-      level: t.String({
-        minLength: 3,
-        maxLength: 50,
-      }),
-    })
-  })
+  .post('/', ({ body }) => createPokemon(body))
   .patch('/:id', () => 'Update Pokemon')
   .delete('/', () => 'Delete Pokemon');
 
